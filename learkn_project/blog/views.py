b31391e9dec7852a35d0c23102995e949e91becb
mc-llz <marcos@llavez.com>
2020-06-29 17:31:56 -0700

    End of Part 6 - User Registration

--- learkn_project/blog/views.py
+++ learkn_project/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from .models import Post
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': Post.objects.all(),
+        'title': 'Home',
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Home</h1>')
+    return render(request, 'blog/about.html')
